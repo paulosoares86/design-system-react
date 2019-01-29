858ecdda62fc27c0280d6209351fa7ccb9e00680
Paulo Soares <phsoares.ita@gmail.com>
2019-01-29 11:31:33 -0200

    Buttons are used as components. Css needs to be extracted

---
+++ src/atoms/Button/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const sizeClasses = {
+  sm: 'btn-sm',
+  lg: 'btn-lg',
+};
+
+export default function Button(props) {
+  const {
+    title,
+    icon,
+    size,
+    color,
+    outline,
+  } = props;
+  const titleSpan = title ? <span className="btn-inner--text">{title}</span> : null;
+  const btnSizeClass = sizeClasses[size] || 'btn-3';
+  const btnColor = color ? `btn-${color}` : 'btn-primary';
+  const btnOutline = outline ? 'btn-outline' : '';
+  const btnIcon = icon ? 'btn-icon' : '';
+  return (
+    <button className={`btn ${btnIcon} ${btnSizeClass} ${btnColor} ${btnOutline}`} type="button">
+      <span className="btn-inner--icon">{icon}</span>
+      {titleSpan}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  icon: PropTypes.element,
+  title: PropTypes.string,
+  size: PropTypes.string,
+  color: PropTypes.string,
+  outline: PropTypes.string,
+};
+
+Button.defaultProps = {
+  icon: false,
+  title: 'Button',
+  size: 'md',
+  color: 'primary',
+  outline: null,
+};
---
+++ src/atoms/Link/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Link(props) {
+  const { to, color, children } = props;
+  const textColor = color ? `text-${color}` : 'text-white';
+  return (
+    <a href={to} className={`btn btn-link ${textColor}`}>{children}</a>
+  );
+}
+
+Link.propTypes = {
+  to: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+Link.defaultProps = {
+  color: null,
+};
