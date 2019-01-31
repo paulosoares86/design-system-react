acf52c9e844c43b2a9c08c8632b4cf05a7e314fa
Paulo Soares <phsoares.ita@gmail.com>
2019-01-31 19:34:17 -0200

    refactoring logo

--- src/molecules/NavBar/index.js
+++ src/molecules/NavBar/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from '../../atoms/Logo';
 
+const LogoLink = () => <a href="/"><Logo color="blue" style={{ height: 27 }} /></a>;
+
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
-        <Logo color="blue" style={{ height: 27 }} />
+        {LogoLink}
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-white" aria-controls="navbar-white" aria-expanded="false" aria-label="Toggle navigation">
           <i className="fas fa-bars" style={{ color: '#112D4E', width: 40 }} />
         </button>
@@ -13,9 +15,7 @@ export default function NavBar() {
           <div className="navbar-collapse-header">
             <div className="row">
               <div className="col-6 collapse-brand">
-                <a href="/">
-                  <Logo color="blue" style={{ height: 27 }} />
-                </a>
+                {LogoLink}
               </div>
               <div className="col-6 collapse-close">
                 <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbar-white" aria-controls="navbar-white" aria-expanded="false" aria-label="Toggle navigation">
