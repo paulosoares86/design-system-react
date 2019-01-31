d686b40f2adfe7f726e9e96f573b40d85fc12848
Paulo Soares <phsoares.ita@gmail.com>
2019-01-31 17:24:16 -0200

    fixing gh pages deployt

--- src/atoms/Logo/index.js
+++ src/atoms/Logo/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Logo(props) {
   const { color } = props;
   const logoColor = color || 'white';
-  const logoUrl = `/images/logo/${logoColor}.png`;
+  const logoUrl = `/assets/images/logo/${logoColor}.png`;
   return <img src={logoUrl} alt="logo" {...props} />;
 }
