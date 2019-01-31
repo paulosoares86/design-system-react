27d531fd2ec012373c5c246f4013e44b77832abb
Paulo Soares <phsoares.ita@gmail.com>
2019-01-31 16:31:49 -0200

    refactoring header

---
+++ src/atoms/Logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Logo(props) {
+  const { color } = props;
+  const logoColor = color || 'white';
+  const logoUrl = `/images/logo/${logoColor}.png`;
+  return <img src={logoUrl} alt="logo" {...props} />;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Header } from './molecules';
 import { Buttons, Inputs } from './templates/stories';
 
 import './scss/argon.scss';
@@ -6,6 +7,7 @@ import './scss/argon.scss';
 export default function ExampleComponent() {
   return (
     <div>
+      <Header />
       <Buttons />
       <Inputs />
     </div>
