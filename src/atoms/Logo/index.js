db17fa4ff9ced41555fa98a5bae6899a1d35b8b2
Paulo Soares <phsoares.ita@gmail.com>
2019-02-01 18:10:12 -0200

    using aws for assets

--- src/atoms/Logo/index.js
+++ src/atoms/Logo/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Logo(props) {
   const { color } = props;
   const logoColor = color || 'white';
-  const logoUrl = `/images/logo/${logoColor}.png`;
+  const logoUrl = `https://s3.amazonaws.com/scubrasil-design/images/logo/${logoColor}.png`;
   return <img src={logoUrl} alt="logo" {...props} />;
 }
